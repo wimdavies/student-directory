21c42fba6b1bab04f81d24e35d8eeae5890d2261
Will Davies <william.johnston.davies@gmail.com>
2023-04-01 16:46:20 +0000

    added comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the student list
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+#then we print the total number of students
 print "Overall we have "
 print "11"
 puts " great students"
