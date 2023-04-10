f8e29ee4a2c8872125c64743086e32911bf97826
Will Davies <william.johnston.davies@gmail.com>
2023-04-10 13:09:36 +0000

    implemented loading students from file

--- directory.rb
+++ directory.rb
@@ -70,13 +70,6 @@ def print_footer
   end
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "9. Exit"
-end
-
 def show_students
   print_header
   print_student_list
@@ -95,6 +88,23 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
+end
+
 def process(selection)
   case selection
   when "1"
@@ -103,6 +113,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # will cause the program to terminate
   else
